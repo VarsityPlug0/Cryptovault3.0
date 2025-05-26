b9565c1f7927432c281385b78346bfc0140c0ea1
VarsityPlug0 <varsityplug0@gmail.com>
2025-05-26 18:34:11 +0200

    Delete varsity_plug directory

--- varsity_plug/helper/templates/helper/admin.py
+++
@@ -1,7 +0,0 @@
-# helper/admin.py
-from django.contrib import admin
-from .models import DocumentUpload, StudentProfile, University
-
-admin.site.register(DocumentUpload)
-admin.site.register(StudentProfile)
-admin.site.register(University)
