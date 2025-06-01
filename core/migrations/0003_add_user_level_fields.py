5e5860f47ca9e1e65291bbeefcdb71dd7c8b15e1
VarsityPlug0 <VarsityPlug0@gmail.com>
2025-06-01 10:01:33 +0200

    Fix: Remove duplicate total_invested field from migration

--- core/migrations/0003_add_user_level_fields.py
+++ core/migrations/0003_add_user_level_fields.py
@@ -9,11 +9,6 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AddField(
             model_name='customuser',
-            name='total_invested',
-            field=models.DecimalField(decimal_places=2, default=0, max_digits=12),
-        ),
-        migrations.AddField(
-            model_name='customuser',
             name='level',
             field=models.IntegerField(default=1),
         ),
